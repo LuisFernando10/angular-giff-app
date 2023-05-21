ebc4901bc4082bdb18f107f898b01f41eefe2be4
Luis Fernando <fernando.chamorro@onetree.com>
2023-05-21 14:58:35 -0500

    Implemented (load) event and loaded logic to show the gifs on lazy mode

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
 })
-export class LazyImageComponent {}
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public imageUrl!: string;
+
+  @Input()
+  public imageAlt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.imageUrl) throw 'URL property is required';
+  }
+
+  onLoad() {
+    console.log('Imagen Czargada');
+    this.hasLoaded = true;
+  }
+}
